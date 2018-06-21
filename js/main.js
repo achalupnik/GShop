4651975352592b91fc6674dafe084c9e8f9c2bcf
BigFuckingHero <rdza222@gmail.com>
2018-06-21 16:38:14 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ $(document).ready(function () {
     var mainNav = $("#mainNav").html();
     var f_form = $("#f_form").html();
     var mobileViewport = window.matchMedia("screen and (max-width: 580px)");
+    if(mobileViewport.matches) {
+        $("#f_form").html("");
+        $("#mainNav").html(mainNav+f_form);
+    } else {
+        $("#f_form").html(f_form);
+        $("#mainNav").html(mainNav);
+    }
     mobileViewport.addListener(function(mq) {
         if(mq.matches) {
             $("#f_form").html("");
